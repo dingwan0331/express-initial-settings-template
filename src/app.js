f9a919296aa0276ed76588e597477c172bbd86c4
dingwan <dingwan0331@gmail.com>
2022-09-13 19:41:52 +0900

    feat: 프로젝트 초기셋팅
    - app.js server.js config파일 작성
    - cors() & logger & ccqp 미들웨어 장착
    - indexRouter 설정
    - ping-pong End Point 설정

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import { default as logger } from "morgan";
+import ccqp from "ccqp";
+
+import { default as indexRouter } from "./indexRouter.js";
+
+const NODE_ENV = process.env.NODE_ENV;
+const loggerSet = {
+  development: "dev",
+  production: "combined",
+};
+const loggerOption = loggerSet[NODE_ENV];
+
+const app = express();
+
+app.use(cors());
+app.use(logger(loggerOption));
+app.use(ccqp);
+app.use(indexRouter);
+
+export default app;
---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SERVER_PORT = process.env.SERVER_PORT || 8000;
+
+export { SERVER_PORT };
