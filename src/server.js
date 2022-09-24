4ec981de009de9de51aead243d1b5a24ce72e29b
dingwan <dingwan0331@gmail.com>
2022-09-24 17:40:27 +0900

    refactor: DB connect 를 app.js에서 server.js로 이동

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import http from "http";
 import { SERVER_PORT } from "./config/index.js";
 import app from "./app.js";
-
+/* DB import
+import { sequelize } from "./models/index.js";
+*/
 const server = http.createServer(app);
 
 const serverStart = () => {
   try {
+    /* DB connect
+    sequelize
+      .sync({ force: false })
+      .then(() => console.log("Connected Database"))
+      .catch((err) => console.error("Database Coneecting Error: ", err));
+    */
     server.listen(SERVER_PORT, () => {
       console.log(`listening on ${SERVER_PORT}!`);
     });
